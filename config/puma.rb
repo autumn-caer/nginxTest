dc6f1e7b2b890713f08dc98b982f10233c1ea2c0
automn-caer <muestra114@gmail.com>
2020-05-06 07:34:06 +0000

    puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -16,6 +16,11 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 # bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 bind "unix:///var/www/nginxTest/shared/tmp/sockets/puma.sock"
+app_dir = File.expand_path("../..", __FILE__)
+# bind "unix://#{app_dir}/tmp/sockets/puma.sock
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+state_path "#{app_dir}/tmp/pids/puma.state"
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
 # bind "unix:///home/vagrant/workspace/nginxTest/tmp/sockets/puma.sock"
 # bind "unix:///var/www/nginxTest/shared/tmp/sockets/puma.sock"
 
@@ -49,9 +54,3 @@ bind "unix:///var/www/nginxTest/shared/tmp/sockets/puma.sock"
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
-
-app_dir = File.expand_path("../..", __FILE__)
-bind "unix://#{app_dir}/tmp/sockets/puma.sock"
-pidfile "#{app_dir}/tmp/pids/puma.pid"
-state_path "#{app_dir}/tmp/pids/puma.state"
-stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
