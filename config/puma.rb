1e0cf780255c415655196020ac95b65be346f702
automn-caer <muestra114@gmail.com>
2020-05-06 02:34:41 +0000

    puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -13,8 +13,8 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+# environment ENV.fetch("RAILS_ENV") { "development" }
+# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 # bind "unix:///var/www/nginxTest/shared/tmp/sockets/puma.sock"
 
 # Specifies the number of `workers` to boot in clustered mode.
