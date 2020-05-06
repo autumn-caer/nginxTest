026ab90c9063640a7edbfa1bbb9eae21ed874e3d
automn-caer <muestra114@gmail.com>
2020-05-06 02:09:01 +0000

    puma.rb modified

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,7 @@ port        ENV.fetch("PORT") { 3000 }
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 # bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
-bind "unix:///var/www/my-app-name/shared/tmp/sockets/puma.sock"
+bind "unix:///var/www/nginxTest/shared/tmp/sockets/puma.sock"
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
