d066185853ebf91e883e3daee740b7b1807020f5
automn-caer <muestra114@gmail.com>
2020-05-06 06:56:18 +0000

    puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -14,7 +14,8 @@ port        ENV.fetch("PORT") { 3000 }
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
-bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+# bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+bind "unix:///var/www/nginxTest/current/shared/tmp/sockets/puma.sock"
 # bind "unix:///home/vagrant/workspace/nginxTest/tmp/sockets/puma.sock"
 # bind "unix:///var/www/nginxTest/shared/tmp/sockets/puma.sock"
 
