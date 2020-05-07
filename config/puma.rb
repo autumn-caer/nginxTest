8916e786d61ae829e9a26491a437cd0037e53b9f
automn-caer <muestra114@gmail.com>
2020-05-07 21:45:34 +0000

    puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -16,8 +16,7 @@ port        ENV.fetch("PORT") { 3000 }
 # environment ENV.fetch("RAILS_ENV") { "development" }
 # bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
 # bind "unix:///var/www/nginxTest/shared/tmp/sockets/puma.sock"
-# app_dir = File.expand_path("../..", __FILE__)
-app_dir = File.expand_path("../../", __FILE__)
+app_dir = File.expand_path("../..", __FILE__)
 bind "unix://#{app_dir}/tmp/sockets/puma.sock"
 pidfile "#{app_dir}/tmp/pids/puma.pid"
 state_path "#{app_dir}/tmp/pids/puma.state"
